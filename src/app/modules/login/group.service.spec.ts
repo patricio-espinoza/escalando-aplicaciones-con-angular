a018d0baa4731ff92b30b77d4409912f562fdde9
Patricio Espinoza Salgado <patricio.espinoza@outlook.com>
2018-12-11 11:21:47 -0300

    Se crea testing de ServiceGroup

--- src/app/modules/login/group.service.spec.ts
+++ src/app/modules/login/group.service.spec.ts
@@ -8,7 +8,7 @@ class HttpClientMock {
   get = jasmine.createSpy();
 }
 
-fdescribe('Group Service', () => {
+describe('Group Service', () => {
     let service: GroupService;
     let httpClientMocK: HttpClientMock;
 
