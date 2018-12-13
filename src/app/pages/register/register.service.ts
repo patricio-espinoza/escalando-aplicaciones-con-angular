366b643effb33dc60fc8438d7bb2b000a9724d9a
Patricio Espinoza Salgado <patricio.espinoza@outlook.com>
2018-12-13 13:06:47 -0300

    Se agrega formulario reactivo y servicios de prueba

--- src/app/pages/register/register.service.ts
+++ src/app/pages/register/register.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { retry } from 'rxjs/operators';
-
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +11,12 @@ export class RegisterService {
 
   constructor(private http: HttpClient) { }
 
-  register(user: User): Observable<User> {
-    return this.http.post<User>(environment.endpoint.register, user).pipe(
-      retry(2),
+  register(user): Observable<void> {
+    return this.http
+    .post<void>(environment.endpoint.register, user)
+    .pipe(
+      retry(2)
     );
   }
 
-}
-
-export interface User {
-  fullName: string;
-  email: string;
-  password: string;
 }
